29e9823b9d104ea53faed13a52211f2c7a2bfb80
LucientZ <koblenzpoisiden@gmail.com>
2022-10-21 16:10:20 -0500

    [ADD] Added mockup of what the nav bar should look like

--- js/main.js
+++ js/main.js
@@ -6,12 +6,15 @@ function elementFromHtml(html){
     return template.content.firstElementChild;
 }
 
-
 function addDefaultHeader(){
     // Adds default header to body.
     const defaultHeader = elementFromHtml(`
     <header class = "default_header">
-        Testing
+        <ul class = "nav">
+            
+            <li><img src = "./img/arrow.png"><a href = "index.html" class = "block-center">Home</a></li>
+            <li><img src = "./img/arrow.png" class = "block-center"><a href = "" class = "block-center">Projects</a></li>
+        </ul>
     </header>
     `);
 
@@ -19,3 +22,10 @@ function addDefaultHeader(){
 }
 
 addDefaultHeader();
+
+
+
+function unhideArrow(){
+    document.getElementById("btn").style.display = 'block'
+}
+
