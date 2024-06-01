d0e54fe400262e6875b8ddc9dfc8a383fe4739ad
LucientZ <koblenzpoisiden@gmail.com>
2024-05-31 22:43:52 -0500

    sections are now partitioned

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@
 var previousScrollPos = window.scrollY;
 $(window).scroll(function(event) {
     const currentPos = window.scrollY;
-    // Compares previous scroll position to the current scroll to determine behavior
-    if(previousScrollPos < currentPos && currentPos > 20){
+    // Compares previous scroll position to the current scroll to determine behavior.
+    // Because of some mobile devices bouncing the scroll when a user scrolls to the top, add an extra argument that makes sure the current position reaches a certain threshhold.
+    if(previousScrollPos < currentPos && currentPos > 30){
         $('.top-elements').css('transform', 'translateY(-200%)');
     }
     else{
         $('.top-elements').css('transform', 'translateY(0%)');
     }
-    previousScrollPos = currentPos
+    previousScrollPos = currentPos;
 });
